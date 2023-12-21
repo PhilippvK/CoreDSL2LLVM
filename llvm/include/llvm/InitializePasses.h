6cd948399f42d6593a4b48e7c0334217850f7074
Mathis Salmen <mathis.salmen@matsal.de>
2023-12-21 12:01:38 +0100

    Pattern gen pass infrastructure
    InitializePasses.h: fix for llvm21 merge conflict

--- llvm/include/llvm/InitializePasses.h
+++ llvm/include/llvm/InitializePasses.h
@@ -347,6 +347,7 @@ LLVM_ABI void initializeWindowsSecureHotPatchingPass(PassRegistry &);
 LLVM_ABI void initializeWinEHPreparePass(PassRegistry &);
 LLVM_ABI void initializeWriteBitcodePassPass(PassRegistry &);
 LLVM_ABI void initializeXRayInstrumentationLegacyPass(PassRegistry &);
+LLVM_ABI void initializePatternGenPass(PassRegistry&);
 
 } // end namespace llvm
 
