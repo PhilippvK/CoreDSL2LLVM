b3477a0df2f93ddab3eda45f309809bc373e9888
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-03-12 17:50:00 +0100

    RISCVTargetTransformInfo.cpp: more GPR32V handling

--- llvm/lib/Target/RISCV/RISCVTargetTransformInfo.cpp
+++ llvm/lib/Target/RISCV/RISCVTargetTransformInfo.cpp
@@ -1388,9 +1388,11 @@ InstructionCost RISCVTTIImpl::getMemoryOpCost(unsigned Opcode, Type *Src,
                                               const Instruction *I) {
   EVT VT = TLI->getValueType(DL, Src, true);
   // Type legalization can't handle structs
-  if (VT == MVT::Other || ST->hasGPR32V())
+  // if (VT == MVT::Other || ST->hasGPR32V()) {
+  if (VT == MVT::Other) {
     return BaseT::getMemoryOpCost(Opcode, Src, Alignment, AddressSpace,
                                   CostKind, OpInfo, I);
+  }
 
   InstructionCost Cost = 0;
   if (Opcode == Instruction::Store && OpInfo.isConstant())
@@ -1402,7 +1404,7 @@ InstructionCost RISCVTTIImpl::getMemoryOpCost(unsigned Opcode, Type *Src,
   // possible accessed by the instruction.  Note that BasicTTI already
   // handles the LT.first term for us.
   if (std::pair<InstructionCost, MVT> LT = getTypeLegalizationCost(Src);
-      LT.second.isVector() && CostKind != TTI::TCK_CodeSize)
+      LT.second.isVector() && CostKind != TTI::TCK_CodeSize && !ST->hasGPR32V())
     BaseCost *= TLI->getLMULCost(LT.second);
   return Cost + BaseCost;
 
@@ -1557,6 +1559,8 @@ InstructionCost RISCVTTIImpl::getVectorInstrCost(unsigned Opcode, Type *Val,
                                                  unsigned Index, Value *Op0,
                                                  Value *Op1) {
   assert(Val->isVectorTy() && "This must be a vector type");
+  if (ST->hasGPR32V())
+    return BaseT::getVectorInstrCost(Opcode, Val, CostKind, Index, Op0, Op1);
 
   if (Opcode != Instruction::ExtractElement &&
       Opcode != Instruction::InsertElement)
