c291d49e8885599027ed6538001b58e0199e9078
Mathis Salmen <mathis.salmen@matsal.de>
2025-04-15 12:15:34 +0200

    small fixes

--- llvm/utils/TableGen/GlobalISelEmitter.cpp
+++ llvm/utils/TableGen/GlobalISelEmitter.cpp
@@ -39,6 +39,7 @@
 #include "Common/GlobalISel/GlobalISelMatchTableExecutorEmitter.h"
 #include "Common/InfoByHwMode.h"
 #include "llvm/ADT/Statistic.h"
+#include "llvm/ADT/StringMap.h"
 #include "llvm/CodeGen/GlobalISel/GIMatchTableExecutor.h"
 #include "llvm/CodeGenTypes/LowLevelType.h"
 #include "llvm/CodeGenTypes/MachineValueType.h"
@@ -2429,6 +2430,7 @@ void GlobalISelEmitter::run(raw_ostream &OS) {
     //#include "../../core_descs/MultiOutput.inc"
     //#include "../../core_descs/Branch.inc"
     #include "../../core_descs/ToycarOpt.inc"
+    //#include "../../core_descs/Store.inc"
   }
   // Look through the SelectionDAG patterns we found, possibly emitting some.
   for (const PatternToMatch &Pat : CGP.ptms()) {
