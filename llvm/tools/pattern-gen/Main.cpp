d00421d0593bd4e52d38500180c11074668b749d
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-10-22 11:46:05 +0200

    [pattern-gen] Write optimized llvm-ir to irOut

--- llvm/tools/pattern-gen/Main.cpp
+++ llvm/tools/pattern-gen/Main.cpp
@@ -128,15 +128,6 @@ int main(int argc, char **argv) {
     auto Mod = std::make_unique<Module>("mod", Ctx);
     auto Instrs = ParseCoreDSL2(Ts, (XLen == 64), Mod.get(), NoExtend);
 
-    if (irOut) {
-      std::string Str;
-      raw_string_ostream OS(Str);
-      OS << *Mod;
-      OS.flush();
-      irOut << Str << "\n";
-      irOut.close();
-    }
-
     if (!SkipVerify)
       if (verifyModule(*Mod, &errs()))
         return -1;
@@ -168,6 +159,10 @@ int main(int argc, char **argv) {
                       .Is64Bit = (XLen == 64)};
 
     optimizeBehavior(Mod.get(), Instrs, irOut, Args);
+
+    if (irOut)
+      irOut.close();
+
     if (PrintIR)
       llvm::outs() << *Mod << "\n";
     if (!SkipFmt)
