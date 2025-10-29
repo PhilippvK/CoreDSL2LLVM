081cc94dfdfc8706f365805b08fbb3ba981ade2b
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-10-29 21:32:15 +0100

    PatternGen: expose --print-mir

--- llvm/lib/CodeGen/GlobalISel/PatternGen.cpp
+++ llvm/lib/CodeGen/GlobalISel/PatternGen.cpp
@@ -1422,6 +1422,10 @@ bool PatternGen::runOnMachineFunction(MachineFunction &MF) {
   MayLoad = 0;
   MayStore = 0;
 
+  if (PatternGenArgs::Args.DumpMIR) {
+    MF.dump();
+  }
+
   std::string InstName = MF.getName().str().substr(4);
   std::string InstNameO = InstName;
   ++PatternGenNumInstructionsProcessed;
