63e5ac421dbaaee3f16b907bb5a21ba3ccafbc9b
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-02-17 09:10:12 +0100

    llvm/lib/CodeGen/GlobalISel/PatternGen.cpp: add missing LLVM_DEBUG

--- llvm/lib/CodeGen/GlobalISel/PatternGen.cpp
+++ llvm/lib/CodeGen/GlobalISel/PatternGen.cpp
@@ -1422,7 +1422,7 @@ bool PatternGen::runOnMachineFunction(MachineFunction &MF) {
   MayStore = 0;
 
   if (PatternGenArgs::Args.DumpMIR) {
-    MF.dump();
+    LLVM_DEBUG(MF.dump());
   }
 
   std::string InstName = MF.getName().str().substr(4);
